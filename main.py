78d69754f4d6212bbd21531d1018e6169aea6deb
Cheserem Titus <50730372+cheseremtitus24@users.noreply.github.com>
2021-10-01 11:32:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -7,9 +5,10 @@ from kivymd.uix.boxlayout import MDBoxLayout
 #Design Our .kv design file
 design= Builder.load_file('design_md_elevationbug.kv')
 
-class MyLayout(MDBoxLayout):
-    pass
-
+class MyLayout(MDBoxLayout): #this bug occurs when inheriting from a kivymd class e.g. we have just inherited from MDBoxLayout class
+    pass # in the kivy definition file when we set an elevation to any element tree class we hit this bug
+# I believe the but lies within the way inheritance of the elevation property is passed down to the children.
+# to Fix this I commented out all the elevation properties within MyLayout child class
 class MdElevationBugApp(MDApp):
     def build(self):
         return MyLayout()
